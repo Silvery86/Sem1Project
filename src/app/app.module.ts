0bf2cae411f028e94791798b2e74927b23f3a1bf
Silvery86 <giang8692@gmail.com>
2022-11-13 06:46:41 +0700

    up page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import { HomepageComponent } from './homepage/homepage.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavigatorComponent } from './navigator/navigator.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+
+const appRoute: Routes = [
+  {path:'',component:HomepageComponent},
+  {path:'aboutus',component:AboutusComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavigatorComponent,
+    AboutusComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(appRoute),
   ],
   providers: [],
   bootstrap: [AppComponent]
